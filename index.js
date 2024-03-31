a551811ed35b3817e75f427809adb2f5c170adee
ernestmusau <ernest.musau@student.moringaschool.com>
2024-03-31 23:14:33 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let testVar = "hi"
+
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Ernest Musau is the champion!";
